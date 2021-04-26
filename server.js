803ec87f049bd8ab9a7b63b9181bba8a544ae274
kris517583 <62744801+kris517583@users.noreply.github.com>
2021-04-26 21:31:31 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
-const Database = require("@replit/database");
 var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: false }) 
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var admin = require("firebase-admin");
+var validUrl = require('valid-url');
+var serviceAccount = require("./serviceAccountKey.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+const db = admin.firestore();
 const app = express();
-const db = new Database();
-db.set("index",0);
-db.set("list",[]);
-db.set("shortlist",[]);
-
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -26,66 +26,80 @@ app.get('/api/hello', function(req, res)
 {
   res.json({ greeting: 'hello API' });
 });
-app.post('/api/shorturl/new',urlencodedParser,function(req, res) 
+app.post('/api/shorturl',urlencodedParser, async function(req, res) 
 {
+  const collections = db.collection('urlshortner').doc('urls');
   var url = req.body.url_new;
   if(url_validator(url))
   {
-    var shorturl = shortener(6);
-    var index = 0;
-    db.get("index").then(value =>{index = value});
-    var list;
-    var shorts;
-    db.get("shortlist").then(value =>{shorts = value});
-    db.get("list").then(value=>{
-      list = value;
+    var result = {};
+    const doc = await collections.get();
+    if (!doc.exists) 
+    {
+      console.log('No such document!');
+      res.json({ error: 'error occured' });
+    } 
+    else 
+    {
+      var shorturl = shortener(6);
+      var index = doc.data()['index'];
+      var list = doc.data()['originalurl'];
+      var shorts = doc.data()['shorturls'];
       if(list.includes(url))
       {
         index = list.indexOf(url);
-        db.get(index).then(value=>{res.json(value)});
+        result["original_url"] = list[index]
+        result["short_url"] = shorts[index]
       }
       else
       {
-        db.set(index,{"original_url":url,"short_url":shorturl}).then(() => 
-        {
-          list.push(url);
-          db.set("index",index+1);
-          db.set("list",list);
-          shorts.push(shorturl);
-          db.set("shortlist",shorts);
-          db.get(index).then(value=>{res.json(value)});
-        });
+        list.push(url);
+        shorts.push(shorturl);
+        index = index+1;
+        const push = await collections.update({originalurl:list,index:index,shorturls:shorts});
+        result["original_url"] = url
+        result["short_url"] = shorturl
       }
-    })
-    .catch((error) => {res.json({error:error})});
-  }
+    }
+      await res.json(result);
+    }
   else
   {
     res.json({ error: 'invalid url' })
   }
 });
-app.get('/api/shorturl/:id?', function(req, res) 
+app.get('/api/shorturl/:id?', async function(req, res) 
 {
   var short_url = req.params.id;
   console.log(short_url);
+  const collections = db.collection('urlshortner').doc('urls');
+  const doc = await collections.get();
   var shorts;
-  db.get("shortlist").then(value =>
+  if (!doc.exists) 
   {
-    shorts = value;
+    console.log('No such document!');
+    res.json({ error: 'error occured' });
+  } 
+  else 
+  {
+    shorts = doc.data()["shorturls"];
     if(shorts.includes(short_url))
     {
         index = shorts.indexOf(short_url);
-        db.get(index).then(value=>
+        if(validUrl.isWebUri(doc.data()["originalurl"][index]))
         {
-          console.log(value.original_url)
-          res.redirect(value.original_url);
-        });
+          res.redirect(doc.data()["originalurl"][index]);
+        }
+        else
+        {
+          res.redirect("https://"+doc.data()["originalurl"][index])
+        }
     }
     else
     {
       res.json({error:'Not Found'})
     }
-  });
+  }
 });
 app.listen(port, function() 
 {
